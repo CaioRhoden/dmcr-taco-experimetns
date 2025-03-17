602c0065dd0c77d7f0dc701d4e0338befd70cfd8
CaioRhoden <caiorhoden@gmail.com>
2025-03-17 17:10:24 +0000

    prepare setup to call utils functions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",  # Name of your package
+    version="0.1",      # Version number
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[  # List of dependencies
+        # Add your dependencies here, e.g., "numpy", "pandas"
+    ],
+
+)
